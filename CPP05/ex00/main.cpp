e0872189972c999dc794f88774d12fab4a10f738
bsirikam <buntakan0703@gmail.com>
2023-12-26 10:39:28 +0700

    Update main

--- CPP05/ex00/main.cpp
+++ CPP05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: buntakansirikamonthip <buntakansirikamonth +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 00:08:42 by bsirikam          #+#    #+#             */
-/*   Updated: 2023/12/26 10:28:52 by buntakansirikamo ###   ########.fr       */
+/*   Updated: 2023/12/26 10:38:37 by buntakansirikamo ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,15 @@
 
 int	main(void)
 {
-    Bureaucrat  b1("b1", 1);
     try
     {
+        Bureaucrat  b1("b1", 2);
+        Bureaucrat  b2("b2", 149);
+
+        std::cout << "b1 = " << b1 << std::endl;
         b1.incrementGrade();
+        std::cout << "b2 = " << b2 << std::endl;
+        b2.decrementGrade();
     }
     catch(const std::exception& e)
     {
