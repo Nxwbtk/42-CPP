a916c263c1bf6b184e79b7ad653ad175183bca84
New Dev <65420147@kmitl.ac.th>
2023-12-29 11:56:22 +0700

    Update Form class implementation

--- CPP05/ex01/Form.cpp
+++ CPP05/ex01/Form.cpp
@@ -6,7 +6,7 @@
 /*   By: bsirikam <bsirikam@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/28 00:15:30 by bsirikam          #+#    #+#             */
-/*   Updated: 2023/12/28 00:15:30 by bsirikam         ###   ########.fr       */
+/*   Updated: 2023/12/29 11:53:58 by bsirikam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,23 @@
 Form::Form(void): _gradeToSign(150), _gradeToExecute(150)
 {
 
+};
+
+Form::~Form(void)
+{
+
+};
+
+Form::Form(const Form& cp): _name(cp._name), _gradeToSign(cp._gradeToSign), _gradeToExecute(cp._gradeToExecute)
+{
+
+};
+
+Form& Form::operator=(const Form &cp)
+{
+    if (this != &cp)
+    {
+        this->_signed = cp._signed;
+    }
+    return (*this);
 };
